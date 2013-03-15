67c5afa847b943f234af1710dc77a595952ad473
naden <n.b@naden.de>
2013-03-15 14:44:07 +0100

    init

---
+++ README.md
@@ -0,0 +1,52 @@
+include.js
+===============================
+
+Advanced include implementation for javascript.
+
+__Include a single file__
+
+    include('test1.js');
+
+    include(['test1.js']);
+
+__Include a single file and execute a callback function after it's loaded__
+
+    include('test1.js', function() {
+     console.log('test1.js loaded');
+    });
+
+    include([
+     [ 'test1.js', function() {
+      console.log('test1.js loaded');
+     }]
+    ]);
+
+__Include two files__
+
+    include('test1.js', 'test2.js');
+
+    include(['test1.js', 'test2.js']);
+
+__Include two files and execute a callback function after they're loaded__
+
+    include('test1.js', 'test2.js', function() {
+     console.log('test1.js and test2.js are loaded');
+    });
+
+    include(['test1.js', 'test2.js'], function() {
+     console.log('init');
+    });
+
+__Include two files and execute a callback function per file__
+
+    include([
+     ['test1.js', function() {
+      console.log('init1');
+     }],
+     ['test2.js', function() {
+      console.log('init2');
+     }]
+    ]);
+
+
+*For a more detailed explanation have a look at this blogpost [Javascript include](http://www.naden.de/blog/javascript-include).*
---
+++ include.js
@@ -0,0 +1,120 @@
+/**
+ * Include function for javascript on speed.
+ *
+ * v0.3 (c) 2006-2013 by Naden Badalgogtapeh <n.b@naden.de>
+ *
+ * http://www.naden.de
+ *
+ */
+
+function include() {
+	if(arguments.length == 0) {
+		return;
+	}
+	
+	var
+		// cast function "arguments" to array
+		arguments = Array.prototype.slice.call(arguments),
+		// optinal callback
+		callback = null,
+		// trigger callback after "file" or "files"
+		callback_trigger = 'file',
+		// the files are stored here
+		files;
+
+	// at first detect, what type of call we are dealing with
+
+	// single file
+	// array of file(s)
+	// array of files + callback per file
+	if(arguments.length == 1) {
+		files = arguments[0];
+	}
+	// files
+	// single file + callback
+	// files + single callback
+	// array of files + single callback
+	// array of files + callback per file
+	// array of files + callback for some files
+	else {
+		// if the last argument is a function ...
+		if(typeof arguments[arguments.length -1] == 'function') {
+			// pop() it off
+			callback = arguments.pop();
+			// set trigger mode
+			callback_trigger = 'files';
+		}
+		// unnest the "files" array
+		files = arguments.length == 1 ? (typeof arguments[0] == 'object' ? arguments[0] : arguments) : arguments;
+	}
+
+	// force "files" to be an array
+	if(typeof files !== 'object') {
+		files = [files];
+	}
+
+	// trigger one callback per file
+	if(callback_trigger == 'file') {
+		// flag indicating if the last callback was "file" bound
+		var perfile = false;
+
+		while(files.length > 0) {
+			var file = files.shift();
+
+			// current file has it's own callback
+			if(typeof file == 'object') {
+				callback = file[1];
+				file = file[0];
+				perfile = true;
+			}
+			// just a regular file
+			else if(perfile) {
+				perfile = false;
+				callback = null;
+			}
+
+			// inject the file
+			_inject(file, callback);
+		}
+	}
+	// trigger the callback after all files are injected
+	else if(callback_trigger == 'files') {
+		_include(files.shift(), callback);
+		
+		// handle our nested callbacks chain
+		function _include(file, callback) {
+			// inject the file + recursively callback
+			_inject(file, function() {
+				if(callback) {
+					if(files.length > 0) {
+						_include(files.shift(), callback);
+					}
+					// final file reached, now call the "callback" and we are done
+					else {
+						callback();
+					}
+				}
+			});
+		}
+	}
+
+	// injects a javascript file to the pages <head> section
+	function _inject(file, callback) {
+		var s = document.createElement('script');
+		s.type = 'text/javascript';
+	  s.src = file + '?' + Math.random().toString().substring(2);
+		// s.async =
+
+		if(callback) {
+			var loaded = false;
+			s.onload = s.onreadystatechange = function() {
+				if(!loaded && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+					loaded = true;
+					callback();
+				}
+			};
+		}
+
+		document.getElementsByTagName('head')[0].appendChild(s);
+	}
+}
